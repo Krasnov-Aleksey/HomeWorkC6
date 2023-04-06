5d869fbb3f95159bfcdf72c7ea445f1ecd0a5e31
aleksey.krasnov <krasnov.80@mail.ru>
2023-04-06 13:19:19 +0300

    Перебор букв'

--- Taskadd1/Program.cs
+++ Taskadd1/Program.cs
@@ -1,88 +1,17 @@
-﻿
-// int Mes (int n)
-// {
-//     if(n==0) 
-//     {
-//         return 0;
-//     }
-//     else
-//     {
-//         Console.WriteLine($"{n} {n-1}");
-//         return n+Mes(n-1);
-//     }
-// }
-
-// Console.Clear();
-// int count=5;
-
-// for(int i=0;i<=count;i++)
-// {
-// Console.WriteLine(Mes(i));
-// }
-
-// void Variants(string lett,int i)
-// {
-//     Console.Write(lett[i]);
-//     Console.Write(lett[i+1]);
-//     Console.WriteLine();
-//     Console.Write(lett[i+1]);
-//     Console.Write(lett[i]);
-//     Console.WriteLine();
-//     Console.Write(lett[i+2]);
-//     Console.Write(lett[i+1]);
-//     //Console.WriteLine();
-// }
-
-// char Variants(char[] lett)
-// {
-//      if(i<=0)
-//      {
-//         return lett
-//      }
-// }
-
-
-// Console.Clear();
-// string lett="IOX";
-// for (int i=0;i<3;i++)
-// {
-//     Variants(lett,0);
-// }
-//Variants(lett,0);
-// for(int i=1;i<0;i--)
-// {
-//     Variants(lett,i);
-// }
-
-//Variants(lett);
-
-// int C (int n, int k)
-// {
-//     if (k==n || k == 0)
-//     {
-//         return 1;
-//     }
-//     if (k != 1)
-//         return C(n-1, k) + C(n-1, k-1)
-//     else
-//         return n;
-// }
+﻿int n=1;
+void FindWords(string alphabet,char[] word, int length=0)
+{
+    if(length==word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for(int i=0;i<alphabet.Length;i++)
+    {
+        word[length]=alphabet[i];
+        FindWords(alphabet,word,length+1);
+    }
+}
 
 Console.Clear();
-string text="IOX";
-// for (int i = 0; i < text.Length; i++)
-// {
-//     //Console.Write(text[i]);
-//     for (int j = 0; j < text.Length; j++)
-//     {
-//         for (int l = 0; l < text.Length; l++)
-//         {
-//             Console.Write(text[i]);
-//             Console.Write(text[j]);
-//             Console.WriteLine(text[l]);
-//         }
-//     }
-//     //Console.WriteLine();
-// }
-
+FindWords("аи",new char[2]);
 
